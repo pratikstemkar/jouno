7265db1290d02a724df73be353569ec0442ae7b9
pratikstemkar <pratikstemkar@gmail.com>
2023-12-02 16:52:52 +0530

    go-blueprint initialization

---
+++ cmd/api/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"jouno/internal/server"
+	"os"
+	"strconv"
+
+	_ "github.com/joho/godotenv/autoload"
+)
+
+func main() {
+
+	server := server.New()
+
+	server.RegisterFiberRoutes()
+	port, _ := strconv.Atoi(os.Getenv("PORT"))
+	err := server.Listen(fmt.Sprintf(":%d", port))
+	if err != nil {
+		panic("cannot start server")
+	}
+}
