001ff89a9a3a790ce13a985c740b8eb5d5a7ff84
pratikstemkar <pratikstemkar@gmail.com>
2023-12-09 15:47:22 +0530

    api group and versioning

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -1,21 +1,8 @@
 package server
 
-import (
-	"github.com/gofiber/fiber/v2"
-)
-
 func (s *FiberServer) RegisterFiberRoutes() {
-	s.App.Get("/", s.HelloWorldHandler)
-	s.App.Get("/health", s.healthHandler)
-}
-
-func (s *FiberServer) HelloWorldHandler(c *fiber.Ctx) error {
-	resp := map[string]string{
-		"message": "Hello World",
-	}
-	return c.JSON(resp)
-}
-
-func (s *FiberServer) healthHandler(c *fiber.Ctx) error {
-	return c.JSON(s.db.Health())
+	api := s.App.Group("/api")
+	v1 := api.Group("/v1")
+	v1.Get("/", s.HelloWorldHandler)
+	v1.Get("/health", s.healthHandler)
 }
