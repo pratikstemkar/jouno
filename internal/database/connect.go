d693b3e2cc14498d7232c250c8b7c188595ef965
pratikstemkar <pratikstemkar@gmail.com>
2023-12-10 16:33:37 +0530

    vercel

--- internal/database/connect.go
+++ internal/database/connect.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"jouno/internal/config"
 	"jouno/internal/model"
-	"strconv"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -12,15 +11,15 @@ import (
 
 func ConnectDB() {
 	var err error
-	p := config.Config("DB_PORT")
-	port, err := strconv.ParseUint(p, 10, 32)
+	// p := config.Config("DB_PORT")
+	// port, err := strconv.ParseUint(p, 10, 32)
 
 	if err != nil {
 		panic("failed to parse database port")
 	}
 
-	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", config.Config("DB_HOST"), port, config.Config("DB_USERNAME"), config.Config("DB_PASSWORD"), config.Config("DB_DATABASE"))
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	// dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", config.Config("DB_HOST"), port, config.Config("DB_USERNAME"), config.Config("DB_PASSWORD"), config.Config("DB_DATABASE"))
+	DB, err = gorm.Open(postgres.Open(config.Config("POSTGRES_URL")), &gorm.Config{})
 
 	if err != nil {
 		panic("failed to connect database")
