dcd390073e2e03acb99793485961f170b1879ce1
pratikstemkar <pratikstemkar@gmail.com>
2023-12-12 14:49:02 +0530

    defer cancel db

--- internal/database/connect.go
+++ internal/database/connect.go
@@ -1,10 +1,12 @@
 package database
 
 import (
+	"context"
 	"fmt"
 	"jouno/internal/config"
 	"jouno/internal/model"
 	"strconv"
+	"time"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -21,10 +23,12 @@ func ConnectDB() {
 
 	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", config.Config("DB_HOST"), port, config.Config("DB_USERNAME"), config.Config("DB_PASSWORD"), config.Config("DB_DATABASE"))
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-
 	if err != nil {
-		panic("failed to connect database")
+		fmt.Printf("%s", err)
 	}
+
+	_, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
 
 	fmt.Println("Connection Opened to Database")
 	DB.AutoMigrate(&model.User{}, model.Role{})
